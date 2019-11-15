d849e3c37ba484aca3530b2379e8ecac9c4da52a
Maxim Anisimov <maximanisimov1@gmail.com>
2019-11-15 23:23:35 +0200

    Created checkCollection method in storageHandler

--- src/components/mixins/storageHandler.js
+++ src/components/mixins/storageHandler.js
@@ -4,6 +4,11 @@ export default {
       return this[itemName].map(el => {
         return el[fieldName];
       });
+    },
+    checkCollectionData(itemName, fieldName, checkValue) {
+      return this[itemName].some(el => {
+        return el[fieldName] === checkValue;
+      });
     }
   }
 };
