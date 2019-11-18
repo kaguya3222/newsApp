6af4c541c3565af0378db5a68eda0af1023dcfa3
Maxim Anisimov <maximanisimov1@gmail.com>
2019-11-18 21:06:12 +0200

    Created news.js vuex module

---
+++ src/store/modules/news.js
@@ -0,0 +1,19 @@
+export default {
+  state: {
+    news: [
+      {
+        title: "Our first news card",
+        smallDescription: "Small description text"
+      },
+      {
+        title: "Our second news card",
+        smallDescription: "Small description text"
+      }
+    ]
+  },
+  getters: {
+    news(state) {
+      return state.news;
+    }
+  }
+};
