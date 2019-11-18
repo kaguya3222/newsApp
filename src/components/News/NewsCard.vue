cb401b6c57061f8cb60ba8429d0129dc5c605aa2
Maxim Anisimov <maximanisimov1@gmail.com>
2019-11-18 22:03:05 +0200

    Created NewsCard.vue

---
+++ src/components/News/NewsCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-card class="d-flex flex-column ml-3 mb-10" min-width="220" height="230">
+    <v-card-title class="flex-grow-2">{{ newsCardData.title }}</v-card-title>
+    <v-card-text class="flex-grow-1">
+      <p>{{ newsCardData.smallDescription }}</p>
+    </v-card-text>
+    <v-card-actions class="flex-grow-2">
+      <v-btn text color="indigo">Learn more</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    newsCardData: Object
+  }
+};
+</script>
