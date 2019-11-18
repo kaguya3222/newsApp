d39f28345ed44279229d45c4981b3d3faa97dda1
Maxim Anisimov <maximanisimov1@gmail.com>
2019-11-18 22:03:38 +0200

    Made layout for news card in Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,24 @@
 <template>
-  <div class="wrapper">
-    <p>Hello</p>
+  <div class="wrapper d-flex flex-wrap justify-center full-height">
+    <app-news-card
+      v-for="(newsCard, index) in news"
+      :key="index"
+      :newsCardData="newsCard"
+    ></app-news-card>
   </div>
 </template>
 
 <script>
-export default {};
+import NewsCard from "./News/NewsCard";
+
+import { mapGetters } from "vuex";
+
+export default {
+  computed: {
+    ...mapGetters(["news"])
+  },
+  components: {
+    "app-news-card": NewsCard
+  }
+};
 </script>
