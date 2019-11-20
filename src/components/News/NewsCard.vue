9c1bea074ba62356f9043cf758115c59d2d6e935
Maxim Anisimov <maximanisimov1@gmail.com>
2019-11-20 22:37:52 +0200

    Refactored displaying in NewsCard.vue

--- src/components/News/NewsCard.vue
+++ src/components/News/NewsCard.vue
@@ -1,8 +1,15 @@
 <template>
-  <v-card class="d-flex flex-column ml-3 mb-10" min-width="220" height="230">
+  <v-card
+    class="d-flex flex-column ml-3 mb-10 align-self-start"
+    max-width="250"
+    style="width: 100%;"
+  >
     <v-card-title class="flex-grow-2">{{ newsCardData.title }}</v-card-title>
-    <v-card-text class="flex-grow-1">
+    <v-card-text min-height="200">
       <p>{{ newsCardData.smallDescription }}</p>
+      <span class="font-weight-bold text-end"
+        >Date: {{ newsCardData.date }}</span
+      >
     </v-card-text>
     <v-card-actions class="flex-grow-2">
       <v-btn text color="indigo">Learn more</v-btn>
