e7a9cf98a38f10b3ab9395493dae20b415c1b5c6
Maxim Anisimov <maximanisimov1@gmail.com>
2019-11-21 19:56:21 +0200

    Changed data layout logic in NewsCard.vue

--- src/components/News/NewsCard.vue
+++ src/components/News/NewsCard.vue
@@ -1,15 +1,13 @@
 <template>
   <v-card
-    class="d-flex flex-column ml-3 mb-10 align-self-start"
+    class="d-flex flex-column ml-3 mb-10 align-self-md-start"
     max-width="250"
     style="width: 100%;"
   >
     <v-card-title class="flex-grow-2">{{ newsCardData.title }}</v-card-title>
     <v-card-text min-height="200">
-      <p>{{ newsCardData.smallDescription }}</p>
-      <span class="font-weight-bold text-end"
-        >Date: {{ newsCardData.date }}</span
-      >
+      <p>{{ briefDescription }}</p>
+      <span class="font-weight-bold text-end">Date: {{ date }}</span>
     </v-card-text>
     <v-card-actions class="flex-grow-2">
       <v-btn text color="indigo">Learn more</v-btn>
@@ -21,6 +19,17 @@
 export default {
   props: {
     newsCardData: Object
+  },
+  computed: {
+    date() {
+      return this.newsCardData.date.slice(0, 10);
+    },
+    briefDescription() {
+      const description = this.newsCardData.briefDescription;
+      return description.length > 100
+        ? description.slice(0, 100) + "..."
+        : description;
+    }
   }
 };
 </script>
