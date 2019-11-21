e66c6fa0fcbe649cb38742d5c162be0dba448e51
Maxim Anisimov <maximanisimov1@gmail.com>
2019-11-21 19:55:38 +0200

    Added button in Home.vue layout

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,18 @@
 <template>
-  <div class="wrapper d-flex  flex-wrap justify-center full-height">
+  <div
+    class="wrapper d-flex flex-wrap full-height justify-center"
+    :class="flexClasses"
+  >
     <app-news-card
       v-for="(newsCard, index) in news"
       :key="index"
       :newsCardData="newsCard"
     ></app-news-card>
+    <div class="btn-holder align-self-center ml-md-10 text-end">
+      <v-btn fab dark color="indigo">
+        <v-icon dark>mdi-plus</v-icon>
+      </v-btn>
+    </div>
   </div>
 </template>
 
@@ -14,11 +22,32 @@ import NewsCard from "./News/NewsCard";
 import { mapGetters } from "vuex";
 
 export default {
+  data() {
+    return {
+      isMobile: false
+    };
+  },
   computed: {
-    ...mapGetters(["news"])
+    ...mapGetters(["news"]),
+    flexClasses() {
+      return {
+        "flex-column": this.isMobile,
+        "align-center": this.isMobile,
+        "flex-row": !this.isMobile
+      };
+    }
   },
   components: {
     "app-news-card": NewsCard
+  },
+  methods: {
+    onResize() {
+      this.isMobile = window.innerWidth < 600;
+    }
+  },
+  mounted() {
+    this.onResize();
+    window.addEventListener("resize", this.onResize, { passive: true });
   }
 };
 </script>
