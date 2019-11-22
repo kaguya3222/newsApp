8fb2999e02a4bae6e778114a7da803bb9553bd86
Maxim Anisimov <maximanisimov1@gmail.com>
2019-11-22 21:00:48 +0200

    Added userMenu in index.js (store)

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,7 @@ import app from "./modules/app.js";
 import reg from "./modules/reg.js";
 import auth from "./modules/auth.js";
 import news from "./modules/news.js";
+import userMenu from "./modules/userMenu.js";
 
 Vue.use(Vuex);
 
@@ -16,6 +17,7 @@ export default new Vuex.Store({
     app,
     reg,
     auth,
-    news
+    news,
+    userMenu
   }
 });
