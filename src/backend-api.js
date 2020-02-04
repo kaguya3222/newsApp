c8ed0db560a26b222a331859d937db8103afa808
kaguya3222 <maksimanisimov322@gmail.com>
2020-02-04 19:42:24 +0200

    Refactored export from backend-api.js

--- src/backend-api.js
+++ src/backend-api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export default axios.create({
+export const AXIOS = axios.create({
   baseURL: "https://spring-boot-rest-api-app.herokuapp.com",
   headers: {
     "Access-Control-Allow-Origin": "http://vuejs-news-app.herokuapp.com/"
