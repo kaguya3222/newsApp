49a88e96449af2fbaebba5ccb5bde1f2a3b781da
kaguya3222 <maksimanisimov322@gmail.com>
2020-02-11 20:27:05 +0200

    Refactored backend-api.js

--- src/backend-api.js
+++ src/backend-api.js
@@ -1,8 +1,32 @@
 import axios from "axios";
 
-export default axios.create({
+const AXIOS = axios.create({
   baseURL: "https://spring-boot-rest-api-app.herokuapp.com",
   headers: {
     "Access-Control-Allow-Origin": "http://vuejs-news-app.herokuapp.com/"
   }
 });
+
+export default {
+  async login({ formData }) {
+    return await AXIOS.post("/login", formData);
+  },
+  async refreshTokens({ refreshTokenData }) {
+    return await AXIOS.post("/refresh", refreshTokenData);
+  },
+  async createNewsCard({ newsCardData }) {
+    return await AXIOS.post("/add", newsCardData);
+  },
+  async deleteNewsCard({ tokenData }) {
+    AXIOS.post(`/delete${this.newsCardData.id}`, tokenData);
+  },
+  async register({ regData }) {
+    return await AXIOS.post("/register", regData);
+  },
+  async checkDataUniqueness({ data, dataTitle }) {
+    return await AXIOS.post(`/check${dataTitle}`, data);
+  },
+  async getNews() {
+    return await AXIOS.get("/getAll");
+  }
+};
