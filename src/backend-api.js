17e401b56db56868356b30c0e577c5488b67f520
kaguya3222 <maksimanisimov322@gmail.com>
2020-02-12 20:38:46 +0200

    Updated api calls

--- src/backend-api.js
+++ src/backend-api.js
@@ -1,25 +1,25 @@
-import Vue from "vue";
+import axios from "./plugins/axios";
 
 export default {
   async login({ formData }) {
-    return await Vue.axios.post("/login", formData);
+    return await axios.post("/login", formData);
   },
   async refreshTokens({ refreshTokenData }) {
-    return await Vue.axios.post("/refresh", refreshTokenData);
+    return await axios.post("/refresh", refreshTokenData);
   },
   async createNewsCard({ newsCardData }) {
-    return await Vue.axios.post("/add", newsCardData);
+    return await axios.post("/add", newsCardData);
   },
   async deleteNewsCard({ tokenData }) {
-    Vue.axios.post(`/delete${this.newsCardData.id}`, tokenData);
+    axios.post(`/delete${this.newsCardData.id}`, tokenData);
   },
   async register({ regData }) {
-    return await Vue.axios.post("/register", regData);
+    return await axios.post("/register", regData);
   },
   async checkDataUniqueness({ data, dataTitle }) {
-    return await Vue.axios.post(`/check${dataTitle}`, data);
+    return await axios.post(`/check${dataTitle}`, data);
   },
   async getNews() {
-    return await Vue.axios.get("/getAll");
+    return await axios.get("/getAll");
   }
 };
