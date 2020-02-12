1af95c1ed379db3d4a4936c2fad39fe92e8e7f4b
kaguya3222 <maksimanisimov322@gmail.com>
2020-02-12 19:53:32 +0200

    Changed api calls

--- src/backend-api.js
+++ src/backend-api.js
@@ -1,25 +1,25 @@
-import axios from "axios";
+import Vue from "vue";
 
 export default {
   async login({ formData }) {
-    return await axios.post("/login", formData);
+    return await Vue.axios.post("/login", formData);
   },
   async refreshTokens({ refreshTokenData }) {
-    return await axios.post("/refresh", refreshTokenData);
+    return await Vue.axios.post("/refresh", refreshTokenData);
   },
   async createNewsCard({ newsCardData }) {
-    return await axios.post("/add", newsCardData);
+    return await Vue.axios.post("/add", newsCardData);
   },
   async deleteNewsCard({ tokenData }) {
-    axios.post(`/delete${this.newsCardData.id}`, tokenData);
+    Vue.axios.post(`/delete${this.newsCardData.id}`, tokenData);
   },
   async register({ regData }) {
-    return await axios.post("/register", regData);
+    return await Vue.axios.post("/register", regData);
   },
   async checkDataUniqueness({ data, dataTitle }) {
-    return await axios.post(`/check${dataTitle}`, data);
+    return await Vue.axios.post(`/check${dataTitle}`, data);
   },
   async getNews() {
-    return await axios.get("/getAll");
+    return await Vue.axios.get("/getAll");
   }
 };
--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,3 @@
 module.exports = {
-  transpileDependencies: ["vuetify"],
-  devServer: {
-    proxy: "https://spring-boot-rest-api-app.herokuapp.com/",
-    port: 8080
-  }
+  transpileDependencies: ["vuetify"]
 };
